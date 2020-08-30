f7d30c37c2767cf534ab9e223e160bac39487cfc
Erick <Ikaros__nymph@hotmail.com>
2020-08-29 20:20:26 -0500

    Agregando informacion de los numero en JavaScript

---
+++ 04-Numeros/js/01-app.js
@@ -0,0 +1,17 @@
+// Numeros 
+
+// Los numero en JavaScript  a diferencia de otros lenguajes se pueden escribir son necesidad de ponerle su valor (int, duble, float, etc)
+let numero = 14;
+let numero2 = 30;
+let numero3 = 20.20;
+let numero4 = -50.65;
+let numero5 = 0.205080;
+
+console.log(numero);
+console.log(numero2);
+console.log(numero3);
+console.log(numero4);
+console.log(numero5);
+
+let numeroN = new Number (100);
+console.log(numeroN);
