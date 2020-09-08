449caa58b2630b3c1907a7c35b318fd381b8410d
Erick <Ikaros__nymph@hotmail.com>
2020-09-08 01:46:37 -0500

    Agregando Objetos: Creando mi primer Objeto

---
+++ 07-Objetos/js/01-app.js
@@ -0,0 +1,19 @@
+// Creando un Objeto
+// TODO Un "Objeto" es como una varienle pero el "Objeto" agrupa todo en una sola variable
+// Para crear un objeto es como una variable pero mas amplia
+// Ejemplo:
+const objeto = { // A esto se le conoce como Object literal
+    valorUno: "Uno", // A esto se le conoce como propiedad o llave del Objeto
+    valorDos: 2,
+    valorTres: true
+}
+console.log(objeto);
+
+// Ejercicio: crear un Objeto del cuerpo Humano
+const cuerpoHumano = {
+    cabello: true,
+    brazos: "Derecho e Izquierdo",
+    piernas: "Derecho e Izquierdo",
+    ojos: 2
+}
+console.log(cuerpoHumano);
