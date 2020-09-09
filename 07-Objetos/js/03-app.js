ae79e8e96d22319bfda9470109980ca5c7418c61
Erick <Ikaros__nymph@hotmail.com>
2020-09-09 01:31:53 -0500

    Objeto: Agregando y quitando informacion fuera de un Objeto

---
+++ 07-Objetos/js/03-app.js
@@ -0,0 +1,15 @@
+// Agregar y eliminar valores de un objeto
+
+const persona = {
+    nombre: "Maiky",
+    edad: "32",
+    vivo: true
+}
+ 
+// Agregando un nuevo valor a un Objeto
+persona.strimer = "si";
+console.log(persona);
+
+// Borrando una variable del Objeto
+delete persona.vivo;
+console.log(persona);
