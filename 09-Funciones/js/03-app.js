76aabb80e3c11ce6c18a97b9d43971b1c348728e
Erick <Ikaros__nymph@hotmail.com>
2020-09-20 02:29:17 -0500

    Funciones: Funciones Nativas de JavaScript e información.

---
+++ 09-Funciones/js/03-app.js
@@ -0,0 +1,10 @@
+// Funciones Nativas de JavaScript (Informacion)
+
+//TODO los lenguajes de programacion cuentan con varias funciones listas para ser usadas a esto se les llama libreria estandar lenguajes como PHP, Phaton, Angular, etc. que tienen entre más de 3000 para su uso mientras que jaja esta entre más de 4000.
+
+// JavaScript no tiene una manera como tal para ser usadas por ejemplo
+alert("Esto es un mensaje y puede ser de error."); // Esto ya es una funcion
+prompt("Cual es tú edad?"); // Esto ya es una funcion
+// Una manera para ver todas las funciones que tiene JavaScript para su uso debemos de escribir "Window" en en navegador.
+
+// Recuerda que cuando veas el nombre sumado el parentecis "parseInt()" esto sera una funcion
