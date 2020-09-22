5a2c7cd89d71be653b8a041c32dd97ecf895c11b
Erick <Ikaros__nymph@hotmail.com>
2020-09-22 02:35:37 -0500

    Estructuras de control: Creando estructura o funcionamiento del "if"

---
+++ 10-Estructuras/js/01-app.js
@@ -0,0 +1,11 @@
+// Creando un if
+
+// En este punto sabemos que un if es una condicion con eso nos referimos a que es una restriccion quedara mas claro connel soguiente ejemplo.
+
+const valor = 1000;
+
+if(valor === 1000){
+    console.log(`Su cuenta es perfecta ${valor}`);
+} else{
+    console.log(`Su cuenta no llego al numero deseado`);
+}
