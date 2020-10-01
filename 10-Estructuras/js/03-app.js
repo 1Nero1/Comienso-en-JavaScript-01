61c7eb673392de0d12e46f7d624a4f178882dd31
Erick <Ikaros__nymph@hotmail.com>
2020-10-01 03:23:45 -0500

    Estructuras de control: Operador Mayor y Menos que

---
+++ 10-Estructuras/js/03-app.js
@@ -0,0 +1,17 @@
+// Operador mayor que (>) y menor que (<)
+
+// Este tipo de operadores nos ayuda para a comparar los valores que nosotros deseamos
+// Ejemplo:
+
+const efectivo = 1000;
+const comparar = 800;
+
+
+// if(efectivo >= comparar) tambien es usado para realizar que no tenga error cuando tengan el mismo valor.
+if(efectivo > comparar){
+    console.log("Usted puede realizar su compra");
+} else{
+    console.log("No puede Realizar la compra");
+}
+
+//REMEMBER Podemos Realizar la estructura como nosotros deseamos ya que es nuestra forma de poder entender más facl cuando estemos creando más código
