2ac91c47ca2fdaf3db68189ab973a1fd45ce19cc
Erick <Ikaros__nymph@hotmail.com>
2020-10-02 02:35:49 -0500

    Estructuras de control: Operador Terniario

---
+++ 10-Estructuras/js/09-app.js
@@ -0,0 +1,16 @@
+//TODO El operador terniario
+// El operador termiario es igual a un if y switch pero de diferente forma en su escritura
+// Ejemplo:
+
+let usuario = true;
+let pago = true;
+
+//       condicion  |       SI     |           NO
+console.log(usuario ? "Bienvenido" : "Por favor, registrese");
+
+// Como se realiza un Operador terniario anidado 
+console.log(usuario ? pago ? "Si puede pagar": "Falta de efectivo" : "Por favor, registrese");
+//                    ||||||||||||||||||||||||||||||||||||||||||||||
+// SEGUNDO OPERADOR TERNIARIO |||
+//          ********************************************************************************
+// PRIMER OPERADOR TERNIARIO ***
