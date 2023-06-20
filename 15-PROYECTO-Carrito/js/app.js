6544123ea98e68e8a938a76d73dc3d150dbc8535
Nero <Ikaros__nymph@hotmail.com>
2023-06-19 18:39:47 -0600

    Se agrego la funcion de agregar productos en el carrito y de eliminarlos del mismos

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -3,26 +3,30 @@ const carrito = document.querySelector('#carrito');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarrito = document.querySelector('#vaciar-carrito');
 const listaCursos = document.querySelector('#lista-cursos');
+let agregarArticulos = [];
 
-// NOTA: Se recomienda crear una funcion dode contenga todos los eventos
+// NOTA: Se recomienda crear una funcion donde contenga todos los eventos
 registroEventos();
 function registroEventos (){
     // Cuando agregas un curso precionando "Agregar al carrito"
-    listaCursos.addEventListener('click',agregarCurso); // Recuerda que no es necesario porner los parentecis en la función (solo en este caso)
+    listaCursos.addEventListener('click', agregarCurso); /* Recuerda que no es necesario porner los parentecis en la función (solo en este caso)*/
 }
 
 // Funciones
     // Agregarcurso
 function agregarCurso(evento){
-    evento.preventDefault(); // Solo lo usamos por el momento ay que nuestros enlaces tienen #
+    evento.preventDefault(); // Solo lo usamos por el momento ya que nuestros enlaces tienen #
     if(evento.target.classList.contains('agregar-carrito')){
-            // Aplicaremos traversing
+        // Aplicaremos traversing
         const cursoSeleccionado = evento.target.parentElement.parentElement;
         leerDatosCurso(cursoSeleccionado);
     }
 }
+/*REMEMBER Que en un proyecto verdadero en lugar de tener # va el enlace "www.ejemplo.com" */
 
-// NOTA: Se recomienda no hacer funciones tan largas (Dividir las secciones.) 
+
+
+// NOTA: Se recomienda no hacer funciones tan largas (Dividir las secciones.)
 
 // Lee el contenido de HTML al que le dimos click y extrae la informacion del curso
 function leerDatosCurso(curso){
@@ -37,5 +41,42 @@ function leerDatosCurso(curso){
         cantidad: 1,
     }
 
-    console.log(datosCurso);
+    //Agrega elementos al carrito
+    agregarArticulos = [...agregarArticulos, datosCurso]
+    /*Se necesita una copia del arreglo anterior pra tener una referencia de los articulos que se agregaron anterior mente */
+
+    console.log(agregarArticulos);
+    
+    carritoHTML();
+}
+
+//Muestra el carrito de compras en el HTML
+function carritoHTML(){
+    
+    //Limpia el HTML
+    limpiarHTML();
+
+    //Recorre el carrito y genera el HTML
+    agregarArticulos.forEach(curso=>{
+        const row = document.createElement('tr');
+        row.innerHTML=`
+            <td>
+                ${curso.titulo}
+            </td>
+        `;
+
+        //Agrega el HTML en el tbody
+        contenedorCarrito.appendChild(row);
+    })
+}
+
+//Elimina los cursos del tbody
+function limpiarHTML(){
+    //Forma lenta 
+    //contenedorCarrito.innerHTML = '';
+
+    //Forma mas rapida
+    while(contenedorCarrito.firstChild){
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild)
+    }
 }
