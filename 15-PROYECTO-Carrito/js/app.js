24832bf20a156e30ea8d9b3c1c46552d37d4e2d5
Nero <Ikaros__nymph@hotmail.com>
2023-06-28 06:17:23 -0600

    Se limpian los comentarios o tips que se crearon durante el desarrollo del proyecto para subirlo a la linea principal.

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -5,19 +5,16 @@ const vaciarCarrito = document.querySelector('#vaciar-carrito');
 const listaCursos = document.querySelector('#lista-cursos');
 let agregarArticulos = [];
 
-// NOTA: Se recomienda crear una funcion donde contenga todos los eventos
 registroEventos();
 function registroEventos (){
     // Cuando agregas un curso precionando "Agregar al carrito"
-    listaCursos.addEventListener('click', agregarCurso); /* Recuerda que no es necesario porner los parentecis en la función (solo en este caso)*/
+    listaCursos.addEventListener('click', agregarCurso);
 
     //Elimina cursos del carrito
     carrito.addEventListener('click',eliminarCurso);
 
     //Vaciar el carrito de compras
     vaciarCarrito.addEventListener('click', () => {
-        //console.log('vaciar carrito')
-
         agregarArticulos = []; //Reseteamos el carrito
 
         limpiarHTML(); //Limpia el carrito (Borra todo el HTML)
@@ -27,38 +24,30 @@ function registroEventos (){
 // Funciones
     // Agregarcurso
 function agregarCurso(evento){
-    evento.preventDefault(); // Solo lo usamos por el momento ya que nuestros enlaces tienen #
+    evento.preventDefault();
     if(evento.target.classList.contains('agregar-carrito')){
         // Aplicaremos traversing
         const cursoSeleccionado = evento.target.parentElement.parentElement;
         leerDatosCurso(cursoSeleccionado);
     }
 }
-/*REMEMBER Que en un proyecto verdadero en lugar de tener # va el enlace "www.ejemplo.com" */
 
     //ELiminaCurso
 function eliminarCurso(e){
     if(e.target.classList.contains("borrar-curso")){
-        //console.log(e.target.getAttribute('data-id'));
         const cursoId = e.target.getAttribute('data-id');
 
         //Elimina del arreglo agregarArticulos por el data-id 
         agregarArticulos = agregarArticulos.filter( curso => curso.id !== cursoId);
 
-        //console.log(agregarArticulos);
         carritoHTML(); // Iterar sobre el carrito y mostrar su HTML
     }
 }
 
-
-// NOTA: Se recomienda no hacer funciones tan largas (Dividir las secciones.)
-
 // Lee el contenido de HTML al que le dimos click y extrae la informacion del curso
 function leerDatosCurso(curso){
-    //console.log(curso);
     // Crear un objeto con el contenido del curso actual
     const datosCurso = {
-        // NOTA: Normalmente lo acemos con "document" pero en este caso curso tiene datos del HTML que conseguimos en el "Traversing"
         imagen: curso.querySelector('img').src,
         titulo: curso.querySelector('h4').textContent,
         precio: curso.querySelector('.precio span').textContent,
@@ -67,27 +56,20 @@ function leerDatosCurso(curso){
     }
 
     //Revisa si un elemento ya existe en el carrito
-        /*REMEMBER SUME permite iterar sobre un arreglo de objetos y verificar si un elemento exite en el */
     const existe = agregarArticulos.some( curso => curso.id === datosCurso.id);
-    //console.log(existe);
     if(existe){ //Actualizamos la cantidad
         const losCursos = agregarArticulos.map(curso =>{
-            //REMEMBER .map crea un nuevo arreglo, el cual va ir iterando sobre todos los elementos del carrito
             if(curso.id === datosCurso.id){
                 curso.cantidad++;
                 
-                return curso; //Retorna el objeto actualizado
+                return curso; 
             }else{
-                return curso; //Retorna los objetos que no son duplicados
+                return curso;
             }
         });
         agregarArticulos = [...losCursos];
     }else{ //Agregamos el curso al carrito
-        //Agrega elementos al carrito
         agregarArticulos = [...agregarArticulos, datosCurso]
-        /*Se necesita una copia del arreglo anterior pra tener una referencia de los articulos que se agregaron anterior mente */
-    
-        console.log(agregarArticulos);
     }
     
     carritoHTML();
@@ -96,12 +78,10 @@ function leerDatosCurso(curso){
 //Muestra el carrito de compras en el HTML
 function carritoHTML(){
     
-    //Limpia el HTML
     limpiarHTML();
 
     //Recorre el carrito y genera el HTML
     agregarArticulos.forEach(curso=>{
-        //Creando un dristrocion para mejorar el codigo
         const {imagen, titulo, precio, cantidad, id}= curso;
 
         const row = document.createElement('tr');
@@ -125,10 +105,6 @@ function carritoHTML(){
 
 //Elimina los cursos del tbody
 function limpiarHTML(){
-    //Forma lenta 
-    //contenedorCarrito.innerHTML = '';
-
-    //Forma mas rapida
     while(contenedorCarrito.firstChild){
         contenedorCarrito.removeChild(contenedorCarrito.firstChild)
     }
